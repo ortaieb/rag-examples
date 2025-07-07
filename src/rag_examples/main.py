318313419eb25339a58c58c06d23d57e5e467b5d
Or Taieb <or.taieb@gmail.com>
2025-07-07 12:11:41 +0100

    adding code to handle embedded vector store and simple main loop

---
+++ src/rag_examples/main.py
@@ -0,0 +1,35 @@
+"""
+  Main call for running the RAG backed LLM
+"""
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector_store import retriever
+
+model = OllamaLLM(model = "llama3.2")
+template = """
+  ou are an exeprt in answering questions about a pizza restaurant
+
+  Here are some relevant reviews: {reviews}
+
+  Here is the question to answer: {question}
+  """
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+def main():
+    """Main entry point for the application."""
+    # print("Hello from my project!  dddd")
+    while(True):
+        print("\n\n~~~~~~~~~~~~~~~~~")
+        question = input("Ask your question (q to quit):")
+        print("\n\n")
+        if question == "q":
+            break
+
+        reviews = retriever.invoke(question)
+        result = chain.invoke({"reviews": reviews, "question": question})
+        print(result)
+
+if __name__ == "__main__":
+    main()
